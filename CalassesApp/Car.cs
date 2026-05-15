0ab7496b78f1532434734db64c639bdd9bd08ed0
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-05-15 16:50:47 +0100

    Classes App

---
+++ CalassesApp/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalassesApp
+{
+    internal class Car
+    {
+        //memeber variable
+        //private hides the variable form other classes starts with _
+        private string _model = "";
+        private string _brand = "";
+        private string _engine = "";
+        private string _engineConfig = "";
+        private bool _isLuxury;
+
+        //Modifying getter and setter
+        public string Brand
+        {
+            get 
+            {
+                if (IsLuxury)
+                {
+                    return _brand + " - Luxury Edition";
+                }
+                else
+                {
+                    {  return _brand; }
+                }
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You enterd NOTHING!");
+                    _brand = "DEFAULTVALUE";
+                }
+                else
+                {
+                    _brand = value;
+                }
+            }
+        }
+        
+        //Getters and setters
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+        public string Model { get => _model; set => _model = value; }
+        public string Engine { get => _engine; set => _engine = value; }
+        public string EngineConfig { get => _engineConfig; set => _engineConfig = value; }
+
+        //Constructor - is called whenever creating an object of given class
+        public Car(string model, string brand, string engine, string engineConfig, bool isLuxury)
+        {
+            Model = model;
+            Brand = brand;
+            Engine = engine;   
+            EngineConfig = engineConfig;
+            IsLuxury = isLuxury;
+
+
+            Console.WriteLine("An " + Brand + " of the model " + Model + " with engine " + Engine + " "+ EngineConfig+" has been created!");
+            
+        }
+    }
+}
---
+++ CalassesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CalassesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an Object of the Clsss car
+            //Careating an instance of the Class car
+            Car audi = new Car("A3","Audi", "4.4L", "V8", true);
+            Car bmw = new Car("M3","BMW", "3.2L", "V6", true);
+
+            //Console.WriteLine("Enter the Brand name: ");
+            //Setting Brand
+            //audi.Brand = Console.ReadLine();
+
+            //Getting Brand
+            Console.WriteLine("You entered " + audi.Brand);
+            Console.WriteLine("You entered " + bmw.Brand);
+            Console.ReadKey();
+        }
+    }
+}
