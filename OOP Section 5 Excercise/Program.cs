3d60869887609a81f4304d7bc993f83d288d9f3a
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-05-20 11:50:28 +0100

    OOP Section 5 Excercise

---
+++ OOP Section 5 Excercise/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace OOP_Section_5_Excercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Miro", 40);
+            person.Greet();
+        }
+    }
+
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        // Public property for Name
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        //Public property for _age
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+        //Constructor to create Person
+        public Person(string name, int age)
+        {
+            _name = name;
+            Age = age;
+        }
+        //Greet method
+        public void Greet()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+    }
+}
