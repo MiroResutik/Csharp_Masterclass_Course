8c50978ae7193373b6f01024e5fdc42afbe7e8a9
miroslav resutik <miroslav.resutik@hotmail.co.uk>
2026-05-27 16:28:38 +0100

    WPF Data Binding

---
+++ DataBinding/Data/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding.Data
+{
+    public class Person
+    {
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
